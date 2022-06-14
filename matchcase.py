cfe82393f95b115da30e9809e64493e3b0b8eba3
edu4py <edu4py@outlook.kr>
2022-06-14 09:30:30 +0900

    Initial commit

---
+++ matchcase.py
@@ -0,0 +1,15 @@
+# First, ask the player about their CPU
+cpuModel = str.lower(input("깃 명령어 입력: "))
+
+# The match statement evaluates the variable's value
+match cpuModel:
+    case "add":  # We test for different values and print different messages
+        print("스테이지 영역에 추가")
+    case "commit":
+        print("깃 저장소에서 버전 관리")
+    case "status":
+        print("현 상황 조회")
+    case "log":
+        print("커밋 이력 조회")
+    case _:  # the underscore character is used as a catch-all.
+        print("다시 입력: ")
